f03aa462649e2b7d111389be35d53838293e83b6
unknown <j.sankalp@starkdigital.in>
2025-01-08 17:09:15 +0530

    basic functionality is done

---
+++ dist/pms-desktop-linux-x64/resources/app/app/src/pages/Activity.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Activity: React.FC = () => {
+    return (
+        <div>
+            <h1>Activity</h1>
+        </div>
+    );
+};
---
+++ dist/pms-desktop-linux-x64/resources/app/main.js
@@ -0,0 +1,35 @@
+const {app, BrowserWindow, ipcMain } = require('electron');
+const url = require('url');
+const path = require('path');
+
+function createMainWindow() {
+    const mainWindow = new BrowserWindow({
+        title: 'PMS',
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: true,
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
+
+    mainWindow.webContents.openDevTools();
+
+    const startUrl = url.format({
+        pathname: path.join(__dirname, './app/build/index.html'),
+        protocol: 'file:',
+        slashes: true
+    });
+
+    mainWindow.loadURL('http://localhost:3000');
+}
+
+app.whenReady().then(createMainWindow);
+
+ipcMain.on('show-modal', (event, arg) => {
+    // if (BrowserWindow.getAllWindows().length === 0) {
+    //     createMainWindow();
+    // }
+    console.log(event,arg);
+});
